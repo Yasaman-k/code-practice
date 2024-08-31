0c2d5fc05549e77e656a332827aed24f952587ea
Yasaman Karbasi <work.yasaman.me@gmail.com>
2024-08-31 11:44:07 +0330

    add performance time func

--- padstart.js
+++ padstart.js
@@ -1,4 +1,6 @@
+const t0 = performance.now();
 const fullNumber = '2034399002125581';
 const last4Digits = fullNumber.slice(-4);
-
+const t1 = performance.now();
 console.log(last4Digits.padStart(5, '*'));
+console.log(t1 - t0);
