a1ad598a5e759c97528cee7a5e01295a0033ea6f
yasaman-k <work.yasaman.me@gmail.com>
2025-01-03 16:10:02 +0330

    add

--- arr.js
+++ arr.js
@@ -396,4 +396,15 @@ const hasPairWithSum = () => {
     console.log('sara');
   }
 };
-hasPairWithSum();
+// hasPairWithSum();
+
+const convertTimestampToDate = (timestamp) => {
+  const date = new Date(timestamp * 1000); // Convert seconds to milliseconds
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+};
+
+console.log(convertTimestampToDate(1733473800));
